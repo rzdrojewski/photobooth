98299f8f9b0608e117fede52ebb48a6250ae0ebe
Rémi ZDROJEWSKI <remi.zdrojewski@metroscope.tech>
2025-09-18 23:26:17 +0200

    layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -375,7 +375,7 @@ export default function Home() {
             {title}
           </h1>
         </header>
-        <main className="flex w-full flex-1 flex-col items-center gap-6">
+        <main className="flex w-full flex-1 flex-col items-center justify-end gap-6 mb-16">
           {!isCountingDown && (
             <div className="flex w-full flex-col items-stretch gap-4 sm:flex-row sm:justify-center">
               <button
